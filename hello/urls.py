3aaca80e5d65c888835ac0b73b5dcf4920196643
Bruno Martins <bscmartins@gmail.com>
2016-05-03 17:51:56 +0100

    fix static files through gunicorn/docker

--- hello/urls.py
+++ hello/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     # Examples:
@@ -8,3 +9,4 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+urlpatterns += staticfiles_urlpatterns()
